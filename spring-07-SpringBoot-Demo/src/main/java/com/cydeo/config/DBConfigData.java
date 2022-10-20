4eed4e60a9575d346ff9758f2a36ff3394eb4894
Sumeye <dalkilincsumeye@gmail.com>
2022-10-20 15:30:22 -0400

    More work added

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/config/DBConfigData.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/config/DBConfigData.java
@@ -1,4 +1,20 @@
 package com.cydeo.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "db")
+@Data
 public class DBConfigData {
+
+    private String userName;
+    private String password;
+    private List<String> type;
+
+
 }
--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -1,6 +1,7 @@
 package com.cydeo.service;
 
 import com.cydeo.config.AppConfigData;
+import com.cydeo.config.DBConfigData;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
@@ -16,11 +17,14 @@ public class CommentService {
 
     private final AppConfigData appConfigData;
 
+    private final DBConfigData dbConfigData;
+
     //We created constructor to provide @Autowired
-    public CommentService(CommentRepository dbCommentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData) {
+    public CommentService(CommentRepository dbCommentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData, DBConfigData dbConfigData) {
         this.dbCommentRepository = dbCommentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
         this.appConfigData = appConfigData;
+        this.dbConfigData = dbConfigData;
     }
 
     public void publishComment(Comment comment){
@@ -34,5 +38,10 @@ public class CommentService {
         System.out.println(appConfigData.getUrl());
     }
 
+    public void printDBConfigData(){
+        System.out.println(dbConfigData.getUserName());
+        System.out.println(dbConfigData.getPassword());
+        System.out.println(dbConfigData.getType());
+    }
 
 }
