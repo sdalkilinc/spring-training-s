7b23683cf1d4ac257700d0371029fe71d71294f9
Sumeye <dalkilincsumeye@gmail.com>
2022-10-20 14:39:51 -0400

    More work added

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring07SpringBootDemoApplication {
+public class CydeoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring07SpringBootDemoApplication.class, args);
+        SpringApplication.run(CydeoApplication.class, args);
     }
+
 
 }
