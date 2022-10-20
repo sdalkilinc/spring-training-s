e53512ef1dc9d593c095095aca94d37254c81029
Sumeye <dalkilincsumeye@gmail.com>
2022-10-20 15:19:46 -0400

    More work added

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.cydeo.service;
 
+import com.cydeo.config.AppConfigData;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
@@ -13,15 +14,25 @@ public class CommentService {
     //We put final, bc then Java reminds you to create constructor
     private final CommentNotificationProxy commentNotificationProxy;
 
+    private final AppConfigData appConfigData;
+
     //We created constructor to provide @Autowired
-    public CommentService(CommentRepository dbCommentRepository, @Qualifier("emailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository dbCommentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData) {
         this.dbCommentRepository = dbCommentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
     }
 
     public void publishComment(Comment comment){
         dbCommentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
+
+    public void printConfigData(){
+        System.out.println(appConfigData.getUserName());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
+    }
+
 
 }
