b0eff4fb9ca850722f60691fb4ed811051ff3225
Sumeye <dalkilincsumeye@gmail.com>
2022-10-22 10:14:29 -0400

    More work added

---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,12 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CarController {
+
+    public String carInfo(){
+
+        return "car/car-info";
+    }
+}
