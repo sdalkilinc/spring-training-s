4eaeb81438d60d2571faf8a885bb35f4d55e34d5
Sumeye <dalkilincsumeye@gmail.com>
2022-10-21 23:00:42 -0400

    More work added

--- spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,14 +1,18 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homePage(){
-        String name = "ABC";
+    public String homePage(Model model){
+
+        model.addAttribute("name", "Cydeo");
+        model.addAttribute("course", "MVC");
+
         return "student/welcome";
     }
 }
