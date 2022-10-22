54c3881367237b64552ea0fc11a467f197f4dad3
Sumeye <dalkilincsumeye@gmail.com>
2022-10-21 21:41:56 -0400

    More work added

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.cydeo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/home")  //for creating en point, we need request mapping
+    public String home(){
+        return "home.html";
+    }
+
+
 }
