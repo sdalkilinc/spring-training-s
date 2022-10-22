2b09197e9a7fae40991eb2f8ee218cd7c4253bce
Sumeye <dalkilincsumeye@gmail.com>
2022-10-21 22:15:47 -0400

    More work added

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -6,10 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")  //for creating en point, we need request mapping
+    @RequestMapping("/home")  //for creating end point, we need request mapping
     public String getHomePage(){
         return "home.html";
     }
+
+    @RequestMapping("/ozzy")
+    public String getHomePage2(){
+        return "home.html";
+    }
+
+    @RequestMapping //nothing and /
+    public String getHomePage3(){
+        return "home.html";
+    }
+
+    @RequestMapping({"/apple", "/orange"})
+    public String getHomePage4(){
+        return "home.html";
+    }
+
 
 
 }
