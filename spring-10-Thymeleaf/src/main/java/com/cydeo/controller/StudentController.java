9cacebe2762870440da2eef70720d75b49092f90
Sumeye <dalkilincsumeye@gmail.com>
2022-10-24 10:40:16 -0400

    More work added

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,9 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
@@ -13,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 //and we don't need to put "/student" in the method level before the end point that we determine
 public class StudentController {
 
-    @RequestMapping("/register") //localhost:8080/student/register
+    @RequestMapping(value = "/register", method = RequestMethod.GET) //localhost:8080/student/register
+    @GetMapping("/register") //line 14 and line 15 are same thing
     public String register(Model model){
 //If you want to see some objects in here, we need to call Model class inside the method
 
@@ -23,7 +22,8 @@ public class StudentController {
     }
 
     //@RequestParam version (look at the end point)
-    @RequestMapping("/welcome") //localhost:9090/student/welcome?name=ozzy
+    @RequestMapping(value = "/welcome", method = RequestMethod.POST) //localhost:9090/student/welcome?name=ozzy
+    @PostMapping("/welcome") //line 25 and 26 are same thing
     public String welcome(@RequestParam String name){
 
         return "student/welcome";
