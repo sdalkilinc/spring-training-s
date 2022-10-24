189f8b4b9dbeac7f8b7380c846ac209c407b53b9
Sumeye <dalkilincsumeye@gmail.com>
2022-10-24 11:28:54 -0400

    More work added

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,7 +4,11 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -13,10 +17,18 @@ public class MentorController {
     @GetMapping("/register") //localhost:8080/mentor/register
     public String register(Model model){
 
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3");
+
+        model.addAttribute("batchList", batchList);
+
         model.addAttribute("mentor", new Mentor());
         return "mentor/mentor-register";
     }
 
+    @PostMapping("/confirm")
+    public String submitForm(){
+        return "mentor/mentor-confirmation";
+    }
 
 
 
