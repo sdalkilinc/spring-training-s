ce46e28f61f1dc30da3bc09e5313ef9b1019532d
Sumeye <dalkilincsumeye@gmail.com>
2022-11-04 20:51:26 -0400

    More work added

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,8 +1,13 @@
 package com.cydeo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class Account {
+
+    @Id
     private Long id;
     private String owner;
     private BigDecimal balance;
