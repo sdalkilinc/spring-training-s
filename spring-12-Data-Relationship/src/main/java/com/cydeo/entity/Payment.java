5363974e71cb60f71580629f475dca9150da8ed0
Sumeye <dalkilincsumeye@gmail.com>
2022-11-05 10:29:29 -0400

    More work added

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -26,8 +26,9 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private Status paymentStatus;
 
-    @OneToOne
-//    @JoinColumn(name = "payment_detail_id")
+//    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "payment_detail_id")
     private PaymentDetail paymentDetail;
 
     public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
