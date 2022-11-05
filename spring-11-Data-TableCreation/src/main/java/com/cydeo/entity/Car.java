27b7fed6c219caf48828f2258d8eaf6c4e305aed
Sumeye <dalkilincsumeye@gmail.com>
2022-11-04 21:29:37 -0400

    More work added

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -1,11 +1,17 @@
 package com.cydeo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Car {
 
     @Id
@@ -15,4 +21,10 @@ public class Car {
     private String make;
     private String model;
 
+    public Car(String make, String model) {
+        //we did not add id in the constructor, bc postgres will add it,
+        // that is why, we also don't add @AllArgsConstructor too
+        this.make = make;
+        this.model = model;
+    }
 }
