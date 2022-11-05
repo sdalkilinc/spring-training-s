03c573b0b33f4b74f24cbd1a5d227bb20137c616
Sumeye <dalkilincsumeye@gmail.com>
2022-11-04 22:04:55 -0400

    More work added

--- spring-11-Data-TableCreation/src/main/resources/schema.sql
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -8,3 +8,22 @@ create table EMPLOYEES(
 
 );
 
+create table departments (
+                             id int
+                             department varchar(100),
+                             division varchar(100),
+                             primary key (id)
+);
+
+create table employees (
+                           employee_id INT,
+                           first_name VARCHAR(50),
+                           last_name VARCHAR(50),
+                           email VARCHAR(50),
+                           hire_date DATE,
+
+                           gender VARCHAR(1),
+                           salary INT,
+
+                           primary key (id)
+);
