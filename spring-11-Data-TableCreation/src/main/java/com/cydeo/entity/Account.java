e11074b8183467c1acb7389634ce06492103f083
Sumeye <dalkilincsumeye@gmail.com>
2022-11-04 20:59:22 -0400

    More work added

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,13 +1,14 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Entity
+@MappedSuperclass//if we don't want to create a table from a class
+// and it has an inheritance relationship with other class, we need to put this annotation
 public class Account {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String owner;
     private BigDecimal balance;
