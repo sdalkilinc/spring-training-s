c604c48fc444c8a7ffb6d8f3d8b4eb0b5440b9b1
Sumeye <dalkilincsumeye@gmail.com>
2022-11-04 21:26:43 -0400

    More work added

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Car.java
@@ -1,7 +1,16 @@
 package com.cydeo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    //if we want to postgres to create a unique id, then we need to add @GeneratedValue annotation
     private Long id;
     private String make;
     private String model;
