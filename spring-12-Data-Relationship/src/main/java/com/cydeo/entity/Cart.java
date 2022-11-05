0fd233e97eb0c00c9532b993a7c4f79382a83e82
Sumeye <dalkilincsumeye@gmail.com>
2022-11-05 11:25:20 -0400

    More work added

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Cart.java
@@ -0,0 +1,21 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
