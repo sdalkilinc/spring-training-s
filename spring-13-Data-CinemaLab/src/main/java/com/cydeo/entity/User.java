3ca763a25bdd1d1480214c663afa3d3bd6252a64
Sumeye <dalkilincsumeye@gmail.com>
2022-11-07 10:16:50 -0500

    More work added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/User.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/User.java
@@ -14,6 +14,7 @@ public class User extends BaseEntity {
     private String email;
     private String password;
     private String username;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_details_id")
     private Account account;
