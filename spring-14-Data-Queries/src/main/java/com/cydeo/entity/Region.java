4c62f948911d5bb479b1fd35a2e3383d7f800288
Sumeye <dalkilincsumeye@gmail.com>
2022-11-07 16:12:32 -0500

    More work added

--- spring-14-Data-Queries/src/main/java/com/cydeo/entity/Region.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Region.java
@@ -1,11 +1,16 @@
 package com.cydeo.entity;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "regions")
+@NoArgsConstructor
+@Data
 public class Region extends BaseEntity {
 
     private String region;
