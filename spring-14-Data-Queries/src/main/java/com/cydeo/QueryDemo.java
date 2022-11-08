0caa99d5b643ba0cd0ffdd085547c16c6c5c6c48
Sumeye <dalkilincsumeye@gmail.com>
2022-11-07 22:40:47 -0500

    More work added

--- spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
 
@@ -34,6 +37,11 @@ public class QueryDemo implements CommandLineRunner {
         System.out.println("findByDivision" + departmentRepository.findByDivision("Health"));
         System.out.println("findByDivisionEndingWith" + departmentRepository.findByDivisionEndingWith("ics"));
         System.out.println("findDistinctTop3ByDivisionContains" + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+
+        System.out.println("--------------EMPLOYEES--------------");
+        System.out.println(employeeRepository.retrieveEmployeeDetail());
+
 
     }
 }
