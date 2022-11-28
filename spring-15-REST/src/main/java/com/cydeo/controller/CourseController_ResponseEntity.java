852c16740e801dc36273b08966c29428eb82be79
Sumeye <dalkilincsumeye@gmail.com>
2022-11-28 09:34:03 -0500

    More work added

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -4,10 +4,7 @@ import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -39,6 +36,17 @@ public class CourseController_ResponseEntity {
     public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("name") String category){
         return ResponseEntity.ok(courseService.getCoursesByCategory(category));
     }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO course){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation", "Create")
+                .body(courseService.createCourse(course));
+    }
+
+
+
 
 
 }
