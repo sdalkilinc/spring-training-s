e537cdec6ea30b7b1af581378ec84955cfdcd1e6
Sumeye <dalkilincsumeye@gmail.com>
2022-11-27 22:24:42 -0500

    More work added

---
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.cydeo.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String email;
+    private String password;
+    private String username;
+    private UserRole role;
+    private AccountDTO account;
+
+}
