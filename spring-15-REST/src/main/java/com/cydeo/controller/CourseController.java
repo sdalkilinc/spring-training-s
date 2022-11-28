413d54d70aaa678ad526b46e9eda72cc18e45bdd
Sumeye <dalkilincsumeye@gmail.com>
2022-11-27 23:46:31 -0500

    More work added

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -3,10 +3,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +32,13 @@ public class CourseController {
     public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
         return courseService.getCoursesByCategory(category);
     }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+
+
 
 }
