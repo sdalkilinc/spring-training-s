274a35a1ccf9ad43fca8606bf9627656f6c0ae04
Sumeye <dalkilincsumeye@gmail.com>
2022-11-28 09:17:50 -0500

    More work added

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -5,6 +5,7 @@ import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +21,23 @@ public class CourseController_ResponseEntity {
         this.courseService = courseService;
     }
 
-    @GetMapping
+    @GetMapping //if you want to create API end point, then we need to use ResponseEntity
     public ResponseEntity<List<CourseDTO>> getAllCourses(){
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .header("Version", "Cydeo.V2")
                 .header("Operation", "Get List")
                 .body(courseService.getCourses());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") long courseId){
+        return ResponseEntity.ok(courseService.getCourseById(courseId));
+    }
+
+    @GetMapping("category/{name}")
+    public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("name") String category){
+        return ResponseEntity.ok(courseService.getCoursesByCategory(category));
     }
 
 
