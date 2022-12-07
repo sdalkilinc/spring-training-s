e4faf295e2c178d105f05a609b584eb7a2c986b4
Sumeye <dalkilincsumeye@gmail.com>
2022-12-06 20:51:22 -0500

    More work added

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
@@ -15,5 +16,19 @@ public class ResponseWrapper {
     private String message;
     private Integer code;
     private Object data;
+
+    public ResponseWrapper (String message, Object data){
+        this.success = true;
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.data = data;
+    }
+
+    public ResponseWrapper (String message){
+        this.success = true;
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.data = data;
+    }
 
 }
